98fbc63c8c43134668c5c8fb58ec34aeca89a555
Mateus <teus.alves.sp@gmail.com>
2023-06-04 23:45:53 -0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export function App() {
         <Header />
       </header>
       <main>
-        <Form />
+        <Form />  
         <Image />
       </main>
     </>
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,9 @@
+import { Input } from '../Inputs/Input'
 import './Form.css'
 
 
 export function Form() {
+
   return (
     <div className='rihts-glb'>
       <div className='text-flot'>
@@ -10,24 +12,12 @@ export function Form() {
           Faça login ou registre-se para começar a construir <br />
           seus projetos ainda hoje.</span>
       </div>
-
       <form className='forms-data'>
-        <div className="inputs-grup">
-          <div className='inputs'>
-            <label className='labels-input'>E-mail</label>
-            <input type="email" className='inputs-iguis' placeholder='Digite seu e-mail'/>
-          </div>
-          <div className='inputs'>
-            <div className='esq-pass'>
-            <span id='span-senha'>Esqueceu a senha?</span>
-            </div>
-            <label className='labels-input'>Senha</label>
-            <input type="password" className='inputs-iguis' placeholder='Digite sua senha'/>
-          </div>
-          
+        <div>
+          <Input />
           <button className='btn-button' type="submit">Entrar</button>
           
-          <span className='text-span'>Ainda não tem uma conta?  <label className='span-label'>Inscreva-se</label></span>
+          <span className='text-span'>Ainda não tem uma conta? <label className='span-label'>Inscreva-se</label></span>
         </div>
       </form>
     </div>
